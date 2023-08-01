8e2b4cee9e22771937b03445b4bdeb56d8d1c2bb
forst09 <dforst09@gmail.com>
2023-08-01 11:01:17 +0300

    start mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,42 @@
-//check webp
-function testWebP(callback) {
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-};
+'use strict';
 
-testWebP(function (support) {
-    if (support) {
-        document.querySelector('html').classList.add('webp');
+document.addEventListener('DOMContentLoaded', function () {
+
+    //check webp
+    function testWebP(callback) {
+        var webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    };
+
+    testWebP(function (support) {
+        if (support) {
+            document.querySelector('html').classList.add('webp');
+        }
+        else {
+            document.querySelector('html').classList.add('no-webp');
+        }
+    });
+
+    //open mobile menu
+    let burger = document.getElementById('burger');
+    let mobileMenu = document.getElementById('mobileMenu');
+    if (burger) {
+        burger.addEventListener('click', function () {
+            mobileMenu.classList.add('active');
+            document.querySelector('body').classList.add('not-scroll');
+        });
     }
-    else {
-        document.querySelector('html').classList.add('no-webp');
+
+    //close mobile menu
+    let close = document.getElementById('closeMobile');
+    if (close) {
+        close.addEventListener('click', function () {
+            mobileMenu.classList.remove('active');
+            document.querySelector('body').classList.remove('not-scroll');
+        });
     }
+
 });
